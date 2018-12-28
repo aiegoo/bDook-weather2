2cee469c69e0f1eff1a7f55a0b00da99927c70aa
aiegoo <eozz21@gmail.com>
2018-12-28 14:27:57 +0900

    on dec 28 from himedia pc weather2 to deploy from github

--- y/js/weather.js
+++ y/js/weather.js
@@ -1,27 +1,56 @@
-$.ajax({
-	type: "get",
-	url: "https://api.openweathermap.org/data/2.5/weather",
-	dataType: "json",
-	data: {
-		id: "1846266",
-		appid: "af147f11a0bdb42f9a8efd152f7bbbf2",
-		units: "metric"
-	},
-	success: function(data) {
-		console.log(data);
-		var html = `
-		<div class="w3-padding">
-			<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">
-		</div>
-		<div class="w3-padding">현재온도:${data.main.temp}도</div>
-		<div class="w3-padding">
-		현재날씨:${data.weather[0].main} / ${data.weather[0].description}</div>
-		<div class="w3-padding">현재바람:${data.wind.speed} / ${data.wind.deg}deg</div>
-		<div class="w3-padding">현재도시:${data.name}</div>
-		`;
-		$("#status").append(html);
-	},
-	error: function(xhr) {
-		console.log(xhr);
-	}
+// af147f11a0bdb42f9a8efd152f7bbbf2
+// api.openweathermap.org/data/2.5/weather?id=2172797
+//api.openweathermap.org/data/2.5/forecast?id=524901
+var log = console.log;
+var urlBase = "https://api.openweathermap.org/data/2.5/";
+$(".tabs > li").click(function(){
+	var n = $(this).index();
+	$(".tabs > li").css({
+		"background-color":"#f8f8f8",
+		"border-bottom":"none",
+		"color":"#333",
+		"font-weight":"normal"
+	});
+	$(this).css({
+		"background-color":"#f60",
+		"border-bottom":"3px solid #390",
+		"color":"#fff",
+		"font-weight":"bold"
+	});
+	$(".conts").hide();
+	$(".conts").eq(n).show(function(){
+		if(n == 0) url = urlBase + "weather";
+		else url = urlBase + "forecast";
+		$.ajax({
+			url: url,
+			type: "get",
+			dataType: "json",
+			data: {
+				id: "1835848",
+				appid: "02efdd64bdc14b279bc91d9247db4722",
+				units: "metric"
+			},
+			success: function(data){
+				if(n == 0) dailyFn(data);
+				else weeklyFn(data);
+			},
+			error: function(xhr){
+				log(xhr);
+			}
+		});
+	});
 });
+$(".tabs > li").eq(0).trigger("click");
+
+function dailyFn(data) {
+	console.log(data);
+	$("#wt_icon").attr("src", "../imges/icon/"+data.weather[0].icon+".png");
+	$("#wt_main").html(data.weather[0].main+" / "+data.weather[0].description);
+	$("#wt_temp").html(data.main.temp+"("+data.main.temp_max+"/"+data.main.temp_min+")");
+	$("#wt_wind").html(data.wind.speed+"/ms ("+data.wind.deg+"deg)");
+	$("#wt_coord").html("위도: "+data.coord.lat+" / 경도: "+data.coord.lon);
+}
+
+function weeklyFn(data) {
+	log("weekly"+data);
+}
